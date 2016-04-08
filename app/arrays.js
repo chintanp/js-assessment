e5308e530452c67576cf32ef7d482bc2c068e106
Chintan <Chintan>
2016-04-07 17:19:34 -0700

    4+ test passed

--- app/arrays.js
+++ app/arrays.js
@@ -3,31 +3,83 @@ exports = (typeof window === 'undefined') ? global : window;
 exports.arraysAnswers = {
 
   indexOf : function(arr, item) {
-
+	var k;
+	
+	if (this == null) {
+		throw new TypeError('"this" is null or not defined');
+	}
+	
+	var o = Object(arr);
+	
+	var len = o.length >>> 0;
+	
+	if (len === 0) {
+		return -1;
+	}
+	
+	var n = 0;
+	
+	while (n < len) {
+		if(o[n] === item ) {
+			return n;
+		}
+		n++;
+	}
+	return -1;
   },
 
   sum : function(arr) {
-
+	  var len = arr.length >>> 0;
+	  var sum = 0;
+	  for (var i = 0; i < len; i++) {
+		  sum = sum + arr[i];
+	  }
+	  return sum;
   },
 
   remove : function(arr, item) {
+	  var len = arr.length >>> 0;
+	  var i = 0;
+	  var newArr = [];
+	  
+	  while (i < len) {
+		  if (arr[i] === item) {
+			  i++;
+			  continue;
+		  }
+		  newArr.push(arr[i]) ;
+		  i++;
+	  }
 
+	  return newArr;
   },
 
   removeWithoutCopy : function(arr, item) {
-
+	  var i = 0;
+	  
+	  while (i < arr.length) {
+		  if (arr[i] === item) {
+			  arr.splice(i, 1);
+			  i = 0;
+		  }
+		  i++;
+	  }
+	  
+	  return arr;
   },
 
   append : function(arr, item) {
-
+	  arr.push(item);
+	  return arr;
   },
 
   truncate : function(arr) {
-
+	  arr.pop();
+	  return arr;
   },
 
   prepend : function(arr, item) {
-
+	  var newArray = [item];
   },
 
   curtail : function(arr) {
